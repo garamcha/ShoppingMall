46ed598c8b2411361d3895f46c65fbe858bf54ab
garamcha <chagaram0610@gmail.com>
2023-06-27 15:18:32 +0900

    기본 동작까지 완성

--- src/Main.java
+++ src/Main.java
@@ -6,25 +6,29 @@ public class Main {
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
         ProductManager p_manager = new ProductManager();
+        boolean res = true;
         int sel = 0;
-        System.out.println("****************");
-        System.out.println("1. 쇼핑몰");
-        System.out.println("2. 고객관리");
-        System.out.println("3. 제품관리");
-        System.out.println("0. 종료");
-        System.out.print("메뉴를 선택하세요. -> ");
-        sel = scn.nextInt(); // 메뉴입력받기
-
-        switch(sel){
-            case 1:
-                break;
-            case 2:
-                break;
-            case 3:
-                p_manager.display();
-                break;
-            default:
-                break;
+        while(res){
+            System.out.println("****************");
+            System.out.println("1. 쇼핑몰");
+            System.out.println("2. 고객관리");
+            System.out.println("3. 제품관리");
+            System.out.println("0. 종료");
+            System.out.print("메뉴를 선택하세요. -> ");
+            sel = scn.nextInt(); // 메뉴입력받기
+            switch(sel){
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    p_manager.display(true);
+                    break;
+                case 0:
+                    break;
+                default:
+                    break;
+            }
         }
     }
 }
